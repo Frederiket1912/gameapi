89fefb806683a2f0ee84e209847e5842dafc964c
Frederiket1912 <44972163+Frederiket1912@users.noreply.github.com>
2020-12-06 14:11:54 +0100

    fixed authentication

--- src/middlewares/basic-auth.ts
+++ src/middlewares/basic-auth.ts
@@ -1,27 +1,30 @@
-var http = require('http')
-var auth = require('basic-auth')
-var compare = require('tsscmp')
-import { Response } from "express"
-import UserFacade from '../facades/userFacadeWithDB';
-
+var http = require("http");
+var auth = require("basic-auth");
+var compare = require("tsscmp");
+import { Response } from "express";
+import UserFacade from "../facades/userFacadeWithDB";
+const debug = require("debug")("user-endpoint");
 
 // Create server
 var authMiddleware = async function (req: any, res: Response, next: Function) {
-  var credentials = auth(req)
+  //var credentials = auth(req);
+  //debug(credentials);
+  let credentials = true;
+  let username = req.body.username;
+  let password = req.body.password;
 
   try {
-    if (credentials && await UserFacade.checkUser(credentials.name, credentials.pass)) {
-      
-      const user = await UserFacade.getUser(credentials.name)
+    if (credentials && (await UserFacade.checkUser(username, password))) {
+      const user = await UserFacade.getUser(username);
       req.userName = user.userName;
       req.role = user.role;
       return next();
     }
   } catch (err) {
-    console.log("UPS")
+    console.log("UPS");
   }
-  res.statusCode = 401
-  res.setHeader('WWW-Authenticate', 'Basic realm="example"')
-  res.end('Access denied')
-}
-export default authMiddleware
+  res.statusCode = 401;
+  res.setHeader("WWW-Authenticate", 'Basic realm="example"');
+  res.end("Access denied");
+};
+export default authMiddleware;
